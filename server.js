5958451aabd157426be36d4ec46c46ea7557b43b
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-05-08 13:41:09 -0400

    updated files

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var expressHandleBars = require("express-handlebars");
+var routes = require("./controllers/burgers_controller.js");
+
+var port = process.env.PORT || 3000;
+
+var app = express();
+
+app.use(express.static("public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.engine("handlebars", expressHandleBars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+app.use("/", routes);
+
+app.listen(port, function(){
+  console.log("listening on port", port);
+});
+ 
